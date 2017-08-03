0a554931c14fb3f6cb7b06e8250665dce6f07b67
Natsagdorj-Choijilsuren <choi@mm.inf.uec.ac.jp>
2017-08-03 17:44:50 +0900

    general structure

--- parameter.py
+++ parameter.py
@@ -2,9 +2,42 @@ import tensorflow as tf
 import numpy as np 
 
 class Parameter:
+    ### general structure ######
     
-    def __init__(self):
+    def __init__(self,depth=[]):
+        self.depth = depth
         
-        pass
-    def pass_parameter(self):        
+        
+    def __call__(self):
+
+        with tf.variable_scope("param"):
+            with tf.variable_scope("fc1"):
+                weight1 = tf.get_variable()
+                bias1 = tf.get_variable()
+
+            with tf.variable_scope("fc2"):
+                weight2 = tf.get_variable()
+                bias2 = tf.get_variable()
+
+            with tf.variable_scope("fc3"):
+                weight = tf.get_variable()
+                bias = tf.get_variable()
+
+            with tf.variable_scope("residual_1"):
+                pass
+            with tf.variable_scope("residual_2"):
+                pass
+            with tf.variable_scope("residual_3"):
+                pass
+            with tf.variable_scope("fc4"):
+                pass
+            with tf.variable_scope("fc5"):
+                pass
+            
+                
+                
+
+        
+
+    
         
